0052105a90d5944a3d68f95dc34ff6ead491117e
Nico53r <rothn@bzz.ch>
2023-12-01 12:52:47 +0100

    weghj

---
+++ bzz.sql
@@ -0,0 +1,10 @@
+create database BZZ_Immo;
+use BZZ_Immo;
+
+create table Schueler (
+    id          INT PRIMARY KEY NOT NULL,
+    name        VARCHAR(100),
+    vorname     VARCHAR(100)
+);
+
+SELECT * from Schueler;
